1c4a93ce075f792d375b946ecd99c71ebcfddb2c
Henrik <norheimhenrik@gmail.com>
2022-02-21 23:29:31 +0100

    Added comments to Army class

--- src/main/java/no/ntnu/iir/wargames/Army.java
+++ src/main/java/no/ntnu/iir/wargames/Army.java
@@ -1,74 +1,120 @@
 package no.ntnu.iir.wargames;
 
-
 import java.util.List;
 import java.util.Objects;
 import java.util.Random;
 
+
+/**
+ * Army is a collection of units
+ *
+ * @author Henrik Norheim Nysæther
+ * @version 21.02.2022
+ */
 public class Army {
+  // class fields
+  private String name;
+  private List<Unit> units;
+  private Random randomNumb;
 
-    private String name;
-    private List<Unit> units;
+  /**
+   * Constructor for army class.
+   *
+   * @param name - name of the army
+   * @param units - list of units
+   */
+  public Army(String name, List<Unit> units) {
+    this.name = name;
+    this.units = units;
+    this.randomNumb = new Random();
+  }
 
-    public Army(String name, List<Unit> units) {
-        this.name = name;
-        this.units = units;
-    }
+  /**
+   * Default constructor for army class.
+   *
+   * @param name - name of the army
+   */
+  public Army(String name) {
+    this.name = name;
+    this.randomNumb = new Random();
+  }
 
-    public Army(String name) {
-        this.name = name;
-    }
+  /**
+   * Gets the army's name.
+   *
+   * @return army name
+   */
+  public String getName() {
+    return name;
+  }
 
-    public String getName() {
-        return name;
-    }
+  /**
+   * Add specified unit to the army.
+   *
+   * @param unit - unit to add
+   */
+  public void add(Unit unit) {
+    this.units.add(unit);
+  }
 
-    public void add(Unit unit) {
-        this.units.add(unit);
-    }
+  /**
+   * Adds all units in given list to the army.
+   *
+   * @param units - list of units
+   */
+  public void addAll(List<Unit> units) {
+    this.units.addAll(units);
+  }
 
-    public void addAll(List<Unit> units) {
-        this.units.addAll(units);
-    }
+  /**
+   * Remove specified unit from army.
+   *
+   * @param unit - unit to remove
+   */
+  public void remove(Unit unit) {
+    this.units.remove(unit);
+  }
 
-    public void remove(Unit unit) {
-        this.units.remove(unit);
-    }
+  /**
+   * checks if the army has any units.
+   *
+   * @return true if the army has units
+   */
+  public boolean hasUnits() {
+    return this.units.size() > 0;
+  }
 
-    public boolean hasUnits() {
-        return this.units.size() > 0;
-    }
+  /**
+   * gets all units in the army
+   *
+   * @return List of units - containing all the units in the army
+   */
+  public List<Unit> getAllUnits() {
+    return this.units;
+  }
 
-    public List<Unit> getAllUnits() {
-        return this.units;
-    }
+  /**
+   * get a random unit from the army
+   *
+   * @return unit - randomly picked
+   */
+  public Unit getRandom() {
+    return this.units.get(
+        this.randomNumb.nextInt(this.units.size())
+    );
+  }
 
-    public Unit getRandom() {
-        Random randomNumb = new Random();
-        return this.units.get(
-                randomNumb.nextInt(this.units.size())
-        );
-    }
-
-    @Override
-    public String toString() {
-        return "Army{" +
-                "name='" + name + '\'' +
-                ", units=" + units +
-                '}';
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Army army = (Army) o;
-        return name.equals(army.name) && Objects.equals(units, army.units);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(name, units);
-    }
+  /**
+   * returns information about the army in string format.
+   *
+   * @return amry information
+   */
+  @Override
+  public String toString() {
+    return "Army{" +
+        "name: '" + name + '\'' +
+        ", units: " + units.size() +
+        '}';
+  }
 }
 
