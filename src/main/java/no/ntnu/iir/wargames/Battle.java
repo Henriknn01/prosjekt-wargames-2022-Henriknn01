893cd93ff438f6dda774803f8630d233f91d96ee
Henrik <norheimhenrik@gmail.com>
2022-02-21 23:12:37 +0100

    Fixed indentation

--- src/main/java/no/ntnu/iir/wargames/Battle.java
+++ src/main/java/no/ntnu/iir/wargames/Battle.java
@@ -1,24 +1,25 @@
 package no.ntnu.iir.wargames;
 
 public class Battle {
-    private Army armyOne;
-    private Army armyTwo;
+  // class fields
+  private Army armyOne;
+  private Army armyTwo;
 
-    public Battle(Army armyOne, Army armyTwo) {
-        this.armyOne = armyOne;
-        this.armyTwo = armyTwo;
-    }
+  public Battle(Army armyOne, Army armyTwo) {
+    this.armyOne = armyOne;
+    this.armyTwo = armyTwo;
+  }
 
-    public Army simulate() {
-        // Todo: create method to simulate battle.
-        return null;
-    }
+  public Army simulate() {
+    // TODO: create method to simulate battle.
+    return null;
+  }
 
-    @Override
-    public String toString() {
-        return "Battle{" +
-                "armyOne=" + armyOne +
-                ", armyTwo=" + armyTwo +
-                '}';
-    }
+  @Override
+  public String toString() {
+    return "Battle{" +
+        "armyOne=" + armyOne.getName() +
+        ", armyTwo=" + armyTwo.getName() +
+        '}';
+  }
 }
