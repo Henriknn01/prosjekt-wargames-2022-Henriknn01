28906f6d008c4feafed73d6a3009c3934da4d231
Henrik <norheimhenrik@gmail.com>
2022-02-21 23:10:54 +0100

    Created main method with placeholder content.

---
+++ src/main/java/no/ntnu/iir/wargames/Main.java
@@ -0,0 +1,14 @@
+package no.ntnu.iir.wargames;
+
+/**
+ * Main application class, responsible for starting the simulation.
+ *
+ * @author Henrik Norheim Nysaether
+ * @version 21.02.2022
+ */
+public class Main {
+  public static void main(String[] args) {
+    // TODO: start simulation
+    System.out.println("Hello world!");
+  }
+}
