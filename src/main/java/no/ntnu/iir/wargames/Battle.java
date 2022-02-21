a0128df4629853fddce722f095a189c888f2b402
Henrik <norheimhenrik@gmail.com>
2022-02-21 04:07:37 +0100

    Crated basic Battle class | still missing the simulate method.

---
+++ src/main/java/no/ntnu/iir/wargames/Battle.java
@@ -0,0 +1,24 @@
+package no.ntnu.iir.wargames;
+
+public class Battle {
+    private Army armyOne;
+    private Army armyTwo;
+
+    public Battle(Army armyOne, Army armyTwo) {
+        this.armyOne = armyOne;
+        this.armyTwo = armyTwo;
+    }
+
+    public Army simulate() {
+        // Todo: create method to simulate battle.
+        return null;
+    }
+
+    @Override
+    public String toString() {
+        return "Battle{" +
+                "armyOne=" + armyOne +
+                ", armyTwo=" + armyTwo +
+                '}';
+    }
+}
