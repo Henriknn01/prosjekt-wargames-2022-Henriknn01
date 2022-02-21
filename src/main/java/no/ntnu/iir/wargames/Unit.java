fea741c8eb1bbfce742836ff34e068747cb5edff
Henrik <norheimhenrik@gmail.com>
2022-02-21 11:09:33 +0100

    Changed indentation from 4 spaces to 2 spaces to comply with CheckStyle

--- src/main/java/no/ntnu/iir/wargames/Unit.java
+++ src/main/java/no/ntnu/iir/wargames/Unit.java
@@ -7,106 +7,107 @@ package no.ntnu.iir.wargames;
  * @version 20.02.2022
  */
 public abstract class Unit {
-    private final String name;
-    private int health;
-    private final int attack;
-    private final int armor;
+  // Class fields
+  private final String name;
+  private int health;
+  private final int attack;
+  private final int armor;
 
-    /**
-     * Unit constructor.
-     *
-     * @param name - name of the unit type
-     * @param health - health of the unit
-     * @param attack - the amount of damage a unit does on attack
-     * @param armor - armor bonus for when the unit gets hit
-     */
-    public Unit(String name, int health, int attack, int armor) {
-        this.name = name;
-        this.health = health;
-        this.attack = attack;
-        this.armor = armor;
-    }
+  /**
+   * Unit constructor.
+   *
+   * @param name - name of the unit type
+   * @param health - health of the unit
+   * @param attack - the amount of damage a unit does on attack
+   * @param armor - armor bonus for when the unit gets hit
+   */
+  protected Unit(String name, int health, int attack, int armor) {
+    this.name = name;
+    this.health = health;
+    this.attack = attack;
+    this.armor = armor;
+  }
 
-    /**
-     * Deals damage to specified unit with the following formula:
-     * health_opponent = health_opponent - ( attack + attackBonus )_this + ( armor + resistBonus)_opponent
-     *
-     * @param opponent - unit to attack
-     */
-    public void attack(Unit opponent) {
-        opponent.health -= (this.attack + this.getAttackBonus()) + (opponent.getArmor() + opponent.getResistBonus());
-    }
+  /**
+   * Deals damage to specified unit with the following formula:
+   * health_opponent = health_opponent - ( attack + attackBonus )_this + ( armor + resistBonus)_opponent
+   *
+   * @param opponent - unit to attack
+   */
+  public void attack(Unit opponent) {
+    opponent.health -= (this.attack + this.getAttackBonus()) + (opponent.getArmor() + opponent.getResistBonus());
+  }
 
-    /**
-     * Returns the name of the unit.
-     *
-     * @return name
-     */
-    public String getName() {
-        return name;
-    }
+  /**
+   * Returns the name of the unit.
+   *
+   * @return name
+   */
+  public String getName() {
+    return name;
+  }
 
-    /**
-     * Returns the health of the unit.
-     *
-     * @return health
-     */
-    public int getHealth() {
-        return health;
-    }
+  /**
+   * Returns the health of the unit.
+   *
+   * @return health
+   */
+  public int getHealth() {
+    return health;
+  }
 
-    /**
-     * Returns the attack points of the unit.
-     *
-     * @return attack points
-     */
-    public int getAttack() {
-        return attack;
-    }
+  /**
+   * Returns the attack points of the unit.
+   *
+   * @return attack points
+   */
+  public int getAttack() {
+    return attack;
+  }
 
-    /**
-     * Returns the armor points of the unit.
-     *
-     * @return armor points
-     */
-    public int getArmor() {
-        return armor;
-    }
+  /**
+   * Returns the armor points of the unit.
+   *
+   * @return armor points
+   */
+  public int getArmor() {
+    return armor;
+  }
 
-    /**
-     * Returns the current health of the unit.
-     * @param health current health
-     */
-    public void setHealth(int health) {
-        this.health = health;
-    }
+  /**
+   * Returns the current health of the unit.
+   * @param health current health
+   */
+  public void setHealth(int health) {
+    this.health = health;
+  }
 
-    /**
-     * Returns the unit in string format with information from every field.
-     *
-     * @return unit information
-     */
-    public String toString() {
-        return ("Name: " + this.name +
-                " | Health: " + this.health +
-                " | Attack: " + this.attack +
-                " | Armor: " + this.armor
-        );
-    }
+  /**
+   * Returns the unit in string format with information from every field.
+   *
+   * @return unit information
+   */
+  public String toString() {
+    return ("Name: " + this.name +
+        " | Health: " + this.health +
+        " | Attack: " + this.attack +
+        " | Armor: " + this.armor
+    );
+  }
 
-    /**
-     * The attack bonus is calculated in different ways for each unit.
-     * Attack bonus is used to calculate how much damage a unit deals on attack.
-     *
-     * @return attack bonus
-     */
-    public abstract int getAttackBonus();
+  /**
+   * The attack bonus is calculated in different ways for each unit.
+   * Attack bonus is used to calculate how much damage a unit deals on attack.
+   *
+   * @return attack bonus
+   */
+  public abstract int getAttackBonus();
 
-    /**
-     * The resist bonus is calculated in different ways for each unit.
-     * Resist bonus is a number used to calculate how resistant a unit is to an attack.
-     *
-     * @return resist bonus
-     */
-    public abstract int getResistBonus();
+  /**
+   * The resist bonus is calculated in different ways for each unit.
+   * Resist bonus is a number used to calculate how resistant a unit is to an attack.
+   *
+   * @return resist bonus
+   */
+  public abstract int getResistBonus();
 }
--- src/main/java/no/ntnu/iir/wargames/units/InfantryUnit.java
+++ src/main/java/no/ntnu/iir/wargames/units/InfantryUnit.java
@@ -12,46 +12,46 @@ import no.ntnu.iir.wargames.Unit;
  * @version 21.02.2022
  */
 public class InfantryUnit extends Unit {
-    /**
-     * Constructor to set all values for the unit.
-     *
-     * @param name - name of the unit type
-     * @param health - health of the unit
-     * @param attack - the amount of damage a unit does on attack
-     * @param armor - armor bonus for when the unit gets hit
-     */
-    public InfantryUnit(String name,int health, int attack, int armor) {
-        super(name, health, attack, armor);
-    }
+  /**
+   * Constructor to set all values for the unit.
+   *
+   * @param name - name of the unit type
+   * @param health - health of the unit
+   * @param attack - the amount of damage a unit does on attack
+   * @param armor - armor bonus for when the unit gets hit
+   */
+  public InfantryUnit(String name,int health, int attack, int armor) {
+    super(name, health, attack, armor);
+  }
 
-    /**
-     * Default constructor used for creating a new infantry unit.
-     * This will set the default values specified in the spec sheet.
-     *
-     * @param name - name of the unit
-     * @param health - health of the unit
-     */
-    public InfantryUnit(String name, int health) {
-        super(name, health, 15, 10);
-    }
+  /**
+   * Default constructor used for creating a new infantry unit.
+   * This will set the default values specified in the spec sheet.
+   *
+   * @param name - name of the unit
+   * @param health - health of the unit
+   */
+  public InfantryUnit(String name, int health) {
+    super(name, health, 15, 10);
+  }
 
-    /**
-     * Calculates the attack bonus for the unit.
-     * Temporarly set to return a static attack bonus.
-     *
-     * @return 2
-     */
-    public int getAttackBonus() {
-        return 2;
-    }
+  /**
+   * Calculates the attack bonus for the unit.
+   * Temporarly set to return a static attack bonus.
+   *
+   * @return 2
+   */
+  public int getAttackBonus() {
+    return 2;
+  }
 
-    /**
-     * Calculates the resist bonus for the unit.
-     * Temporarly set to return a static attack bonus.
-     *
-     * @return 1
-     */
-    public int getResistBonus() {
-        return 1;
-    }
+  /**
+   * Calculates the resist bonus for the unit.
+   * Temporarly set to return a static attack bonus.
+   *
+   * @return 1
+   */
+  public int getResistBonus() {
+    return 1;
+  }
 }
