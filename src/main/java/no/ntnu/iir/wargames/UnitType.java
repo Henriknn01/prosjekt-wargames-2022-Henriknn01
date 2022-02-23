5d7c9164eaf9ef3e6f6bc365718cbd39e231a58d
Henrik <norheimhenrik@gmail.com>
2022-02-23 09:38:37 +0100

    Javadoc update

--- src/main/java/no/ntnu/iir/wargames/UnitType.java
+++ src/main/java/no/ntnu/iir/wargames/UnitType.java
@@ -2,6 +2,9 @@ package no.ntnu.iir.wargames;
 
 /**
  * Different types of units used in application.
+ *
+ * @author Henrik Norheim Nysæther
+ * @version 21.02.2022
  */
 public enum UnitType {
   CAVALRY,
