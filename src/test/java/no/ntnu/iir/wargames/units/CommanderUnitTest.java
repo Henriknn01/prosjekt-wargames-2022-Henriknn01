2f5f370ae36e31377396ca6d76c71c46e234f4a4
Henrik <norheimhenrik@gmail.com>
2022-02-23 03:46:04 +0100

    Created unit tests for CommanderUnit class

---
+++ src/test/java/no/ntnu/iir/wargames/units/CommanderUnitTest.java
@@ -0,0 +1,25 @@
+package no.ntnu.iir.wargames.units;
+
+import no.ntnu.iir.wargames.Unit;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CommanderUnitTest {
+    private Unit commanderUnit;
+
+    @BeforeEach
+    void setUp() {
+        commanderUnit = new CommanderUnit("Commander", 100);
+    }
+
+    // Checks if the constructor sets the correct values
+    @Test
+    void constructorTest() {
+        assertEquals(commanderUnit.getName(), "Commander");
+        assertEquals(commanderUnit.getHealth(), 100);
+        assertEquals(commanderUnit.getAttack(), 25);
+        assertEquals(commanderUnit.getArmor(), 15);
+    }
+}
